25d74c900b075a81086e6af131cbcb5d4aecbe66
luanneves07 <luannevessilva@gmail.com>
2019-10-27 14:11:22 -0300

    GOBA-19001 > Cria configuracoes de migrations
    Resolver conexao com o banco de dados via docker [Falha por conta de conexao TCP na porta padrao]

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,12 @@
 module.exports = {
-  // Para utilizar este dialeto foi necessario instalar pg e pg-hstore
-  dialect: 'postgres',
-  // Configuracoes do banco de dados
-  host: 'localhost',
+  dialect: 'postgres', // Para utilizar este dialeto foi necessario instalar pg e pg-hstore
+  host: 'localhost', // Configuracoes do banco de dados
   username: 'postgres',
   password: 'docker',
   databse: 'gobarber',
-  // Define algumas funcoes que o banco de dados vai utilizar por default
   define: {
-    timestamps: true,
-    // Pardoniza a nemenclatura das tabelas com _ no lugar de camelCase
-    underscored: true,
+    timestamps: true, // Garante as colunas createdAt e updatedAt automaticamente
+    underscored: true, // Pardoniza a nemenclatura das tabelas com _ no lugar de camelCase
     underscoredAll: true,
   },
 };
