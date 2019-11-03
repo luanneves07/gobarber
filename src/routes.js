39b5126436b9e784b459643ad7bc3dbe1ec84d5b
luanneves07 <luannevessilva@gmail.com>
2019-11-03 18:05:47 -0300

    GOBA-19002 > Cria controller para providers
    - Adiciona campo virtual dentro de Files para retornar o path da imagem
    - Adiciona middleware de path estático em APP para provisionar os links da imagem sem precisar de token (Para o frontEnd)
    - Cria rota de providers
    - Nomeia chave estrangeira com 'as'
    - Faz filtro dentro do findAll dos models ao consultar o banco

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionControler';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/providers', ProviderController.index);
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
