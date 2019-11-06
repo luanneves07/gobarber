a3d1b0a9566cf3790d6d65baaa680db244bde07a
luanneves07 <luannevessilva@gmail.com>
2019-11-05 23:17:06 -0300

    GOBA-19002 > Cria rota index para listagem de agenda
    - Inicializa listagem de agendamentos para provedores de serviço

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionControler';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import AppointmentController from './app/controllers/AppointmentController';
 
@@ -24,9 +25,11 @@ routes.get('/providers', ProviderController.index);
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
