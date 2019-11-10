c5a961813207eb30716b12e47a46046a12ee628d
luanneves07 <luannevessilva@gmail.com>
2019-11-10 13:40:19 -0300

    GOBA-19002 > Cria feature para enviar e-mail após o cancelamento
    - Instala o nodeMailer
    - Cria class Mail para gerenciar o envio de emails
    - Cria conta no mailtrap.io
    - Cria configuracoes default para envio de email
    - Envia o e-mail apos o cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: '',
+  auth: {
+    user: '0db66ac5733c4a',
+    pass: 'a094a809ee5691',
+  },
+  default: {
+    from: 'Luan Neves <noreply@gobarber.com>',
+  },
+};
