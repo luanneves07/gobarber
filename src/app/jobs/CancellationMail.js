a5b29a5afbd578289013a608875220392dcfd459
luanneves07 <luannevessilva@gmail.com>
2019-11-13 01:08:31 -0300

    GOBA-19002 > Configura sistema de filas
    - Configura o bee-queue para processar os jobs
    - Cria script queue para fazer o processamento das filas
    - Adiciona bee-queue

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,37 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+/**
+ * Cria o job de envio de e-mails
+ */
+class CancellationMail {
+  get key() {
+    // Chave [unica]
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A fila executou');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
